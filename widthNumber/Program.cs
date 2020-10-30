8b27f1378456c6e48498dc818bb8973497947461
brojlibrojli <brojlibrojli@gmail.com>
2020-10-30 21:09:33 +0100

    firstCommit

--- widthNumber/Program.cs
+++ widthNumber/Program.cs
@@ -7,37 +7,37 @@ namespace widthNumber
         static void Main(string[] args)
         {
             Console.Write("Enter one digit to width it: ");
-            var val = (Console.ReadLine());
+            char charToWigth;
+            bool isChar = char.TryParse(Console.ReadLine(), out charToWigth);
+            while (!isChar)
+            {
+                Console.Write("Enter one digit to width it: ");
+                isChar = char.TryParse(Console.ReadLine(), out charToWigth);
+            }
 
-            Console.Write("Enter the number of desired width: ");
-            int desiredWidth = int.Parse(Console.ReadLine());
+            Console.Write("Enter the number (1-99) of desired width: ");
+            
+            int desiredWidth=0;
+            bool isNumber = int.TryParse(Console.ReadLine(), out desiredWidth);
 
+            while (!isNumber || desiredWidth>99)
+            {
+                Console.WriteLine("Enter the number from 1 to 99");
+                isNumber = int.TryParse(Console.ReadLine(), out desiredWidth);
+            }
+
+            desiredWidth = Math.Abs(desiredWidth);
             int Width = desiredWidth; // if not that new int, loop will be shorter
+
 
             for (int i = 0; i < desiredWidth; i++)
             {
                 for (int j = Width--; j > 0; j--)
                 {
-                    Console.Write(val + " ");
+                    Console.Write(charToWigth);
                 }
                 Console.WriteLine();
             }
-            //  Przetestuj skrajne przypadki, co jeśli wpiszemy:
-
-            //   - liczbę ujemną (dziala)
-
-            //   - coś co nie jest liczbą
-            //  System.FormatException: 'Input string was not in a correct format.' (w przypadku int?)
-
-            //   - ogromną liczbę, np. 9999999999999999999999999999999 
-            //    System.OverflowException: 'Value was either too large or too small for an Int32.' (w przypadku int?)
-
-            //   - liczbę 0 (dziala)
-
-            //   - liczbę zmiennoprzecinkową, np. 2.5
-            //    System.FormatException: 'Input string was not in a correct format.' (w przypadku int?)
-
-            //  Wszystkie wypisane przypadki działają w programie (w przypadku var)
         }
     }
 }
